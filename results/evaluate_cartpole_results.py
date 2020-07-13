3d2159f05615516e43e49aba462a6fabfd4b60e6
thlautenschlaeger <thomas.lautenschlaeger@hotmail.de>
2020-07-13 19:17:30 +0200

    finalized evaluation plots

--- results/evaluate_cartpole_results.py
+++ results/evaluate_cartpole_results.py
@@ -8,29 +8,18 @@ import seaborn as sns
 option_sac = torch.load('option_sac_cartpole100Hz_experiments.pkl')
 sac = torch.load('sac_cartpole100Hz_experiments.pkl')
 
-os_rewards = np.zeros((len(option_sac[0]['J_results'])))
-s_rewards = np.zeros((len(sac[0]['J_results'])))
 os_means = np.zeros((len(option_sac[0]['J_results'])))
 s_means = np.zeros((len(sac[0]['J_results'])))
 os_std = np.zeros((len(option_sac[0]['J_results'])))
 s_std = np.zeros((len(sac[0]['J_results'])))
 
-# for i in range(len(option_sac)):
-#     for j in range(os_rewards.shape[0]):
-#         os_rewards[j] += option_sac[i]['J_results'][j]['J_mean']
-#         s_rewards[j] += sac[i]['J_results'][j]['J_mean']
-
-# os_rewards /= len(option_sac[0]['J_results'])
-# s_rewards /= len(sac[0]['J_results'])
-
-for j in range(os_rewards.shape[0]):
+for j in range(os_means.shape[0]):
     os_tmp = [option_sac[i]['J_results'][j]['J_mean'] for i in range(len(option_sac))]
     s_tmp = [sac[i]['J_results'][j]['J_mean'] for i in range(len(option_sac))]
     os_means[j] = np.mean(os_tmp)
     s_means[j] = np.mean(s_tmp)
     os_std[j] = np.std(os_tmp)
     s_std[j] = np.std(s_tmp)
-
 
 fig, ax = plt.subplots()
 ax.grid(alpha=0.5, linestyle='-')
@@ -46,7 +35,5 @@ ax.set_title("Cartpole100Hz - evaluation")
 ax.legend()
 ax.set_xlabel('Episode')
 ax.set_ylabel('Expected return')
-# plt.plot(os_means, color='green')
-# plt.plot(s_means, color='orange')
 
 plt.show()
