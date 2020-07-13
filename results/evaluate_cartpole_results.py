8e98e21667da03715524a7d0684843bf68efd24b
thlautenschlaeger <thomas.lautenschlaeger@hotmail.de>
2020-07-13 11:49:50 +0200

    beatified evaluation plots

--- results/evaluate_cartpole_results.py
+++ results/evaluate_cartpole_results.py
@@ -1,22 +1,52 @@
 import matplotlib.pyplot as plt
 import torch
 import numpy as np
+import seaborn as sns
+
+# sns.set()
 
 option_sac = torch.load('option_sac_cartpole100Hz_experiments.pkl')
 sac = torch.load('sac_cartpole100Hz_experiments.pkl')
 
 os_rewards = np.zeros((len(option_sac[0]['J_results'])))
 s_rewards = np.zeros((len(sac[0]['J_results'])))
+os_means = np.zeros((len(option_sac[0]['J_results'])))
+s_means = np.zeros((len(sac[0]['J_results'])))
+os_std = np.zeros((len(option_sac[0]['J_results'])))
+s_std = np.zeros((len(sac[0]['J_results'])))
 
-for i in range(len(option_sac)):
-    for j in range(os_rewards.shape[0]):
-        os_rewards[j] += option_sac[i]['J_results'][j]['J_mean']
-        s_rewards[j] += sac[i]['J_results'][j]['J_mean']
+# for i in range(len(option_sac)):
+#     for j in range(os_rewards.shape[0]):
+#         os_rewards[j] += option_sac[i]['J_results'][j]['J_mean']
+#         s_rewards[j] += sac[i]['J_results'][j]['J_mean']
 
-os_rewards /= len(option_sac[0]['J_results'])
-s_rewards /= len(sac[0]['J_results'])
+# os_rewards /= len(option_sac[0]['J_results'])
+# s_rewards /= len(sac[0]['J_results'])
 
-plt.plot(os_rewards, color='green')
-plt.plot(s_rewards, color='orange')
+for j in range(os_rewards.shape[0]):
+    os_tmp = [option_sac[i]['J_results'][j]['J_mean'] for i in range(len(option_sac))]
+    s_tmp = [sac[i]['J_results'][j]['J_mean'] for i in range(len(option_sac))]
+    os_means[j] = np.mean(os_tmp)
+    s_means[j] = np.mean(s_tmp)
+    os_std[j] = np.std(os_tmp)
+    s_std[j] = np.std(s_tmp)
+
+
+fig, ax = plt.subplots()
+ax.grid(alpha=0.5, linestyle='-')
+ax.plot(os_means, color='green', label='rARHMM SAC')
+ax.fill_between(np.arange(os_means.shape[0]), os_means - os_std, os_means + os_std, alpha=0.2, color='green',
+                )
+
+ax.plot(s_means, color='orange', label='SAC')
+ax.fill_between(np.arange(s_means.shape[0]), s_means - s_std, s_means + s_std, alpha=0.2, color='orange',
+                )
+
+ax.set_title("Cartpole100Hz - evaluation")
+ax.legend()
+ax.set_xlabel('Episode')
+ax.set_ylabel('Expected return')
+# plt.plot(os_means, color='green')
+# plt.plot(s_means, color='orange')
 
 plt.show()
--- results/evaluate_pendulum_results.py
+++ results/evaluate_pendulum_results.py
@@ -7,16 +7,28 @@ sac = torch.load('sac_pendulum100Hz_experiments_tanh.pkl')
 
 os_rewards = np.zeros((len(option_sac[0]['J_results']) - 1))
 s_rewards = np.zeros((len(sac[0]['J_results']) - 1))
+os_means = np.zeros((len(option_sac[0]['J_results']) - 1))
+s_means = np.zeros((len(sac[0]['J_results']) - 1))
+os_std = np.zeros((len(option_sac[0]['J_results']) - 1))
+s_std = np.zeros((len(sac[0]['J_results']) - 1))
 
-for i in range(len(option_sac)):
-    for j in range(os_rewards.shape[0]):
-        os_rewards[j] += option_sac[i]['J_results'][j + 1]['J_mean']
-        s_rewards[j] += sac[i]['J_results'][j + 1]['J_mean']
+# for i in range(len(option_sac)):
+#     for j in range(os_rewards.shape[0]):
+#         os_rewards[j] += option_sac[i]['J_results'][j + 1]['J_mean']
+#         s_rewards[j] += sac[i]['J_results'][j + 1]['J_mean']
 
-os_rewards /= (len(option_sac[0]['J_results']) - 1)
-s_rewards /= (len(sac[0]['J_results']) - 1)
+for j in range(os_rewards.shape[0]):
+    os_tmp = [option_sac[i]['J_results'][j + 1]['J_mean'] for i in range(len(option_sac))]
+    s_tmp = [sac[i]['J_results'][j + 1]['J_mean'] for i in range(len(option_sac))]
+    os_means[j] = np.mean(os_tmp)
+    s_means[j] = np.mean(s_tmp)
+    os_std[j] = np.std(os_tmp)
+    s_std[j] = np.std(s_tmp)
 
-plt.plot(os_rewards, color='green')
-plt.plot(s_rewards, color='orange')
+# os_rewards /= (len(option_sac[0]['J_results']) - 1)
+# s_rewards /= (len(sac[0]['J_results']) - 1)
+
+plt.plot(os_means, color='green')
+plt.plot(s_means, color='orange')
 
 plt.show()
